bdcb02f5ca3be39becf24bd0a78b3a50de99bae8
notrexxx <notrexx15@gmail.com>
2021-04-01 17:45:07 -0400

    Add edit controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "users#new"
  
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :update, :edit]
+  get 'users/:id', to: 'users#edit'
 end
