b2faa56adcfe266513de202135965fa01afa1811
notrexxx <notrexx15@gmail.com>
2021-04-01 15:49:37 -0400

    Add new user controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    
+    belongs_to :user, required: false
   
     validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
-    validates :email, presence: false, length: { minimum: 8, maximum: 50 }
-    validates :password, presence: false, length: { minimum: 8, maximum: 20 }
+    validates :email, presence: true, length: { minimum: 8, maximum: 50 }
+    validates :password, presence: true, length: { minimum: 1, maximum: 20 }
   end
