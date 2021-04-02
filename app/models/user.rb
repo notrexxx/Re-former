6dd906f2cfcd4114f42ac87e6fdc812b4a58bad1
notrexxx <notrexx15@gmail.com>
2021-04-02 05:54:43 -0400

    Remove route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  belongs_to :user, required: false
-
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, length: { minimum: 8, maximum: 50 }
   validates :password, presence: true, length: { minimum: 1, maximum: 20 }
